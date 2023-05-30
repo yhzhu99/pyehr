0d0dab9d3662958202abce0af053e8386a06f17e
yhzhu99 <yhzhu99@gmail.com>
2023-05-31 05:28:11 +0800

    feat(plot): draw es / osmae trend in each threshold setting

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -28,8 +28,18 @@ def get_all_metrics(preds, labels, task, los_info):
         y_true_los = reverse_los(labels[:,1], los_info)
         return get_regression_metrics(y_pred_los, y_true_los)
     elif task == "multitask":
-        y_pred_los = reverse_los(preds[:,1], los_info)
-        y_true_los = reverse_los(labels[:,1], los_info)
-        return get_binary_metrics(preds[:,0], labels[:,0]) | get_regression_metrics(y_pred_los, y_true_los) | osmae_score(labels[:,0], y_true_los, preds[:,0], y_pred_los, large_los, threshold) | es_score(labels[:,0], y_true_los, preds[:,0], threshold)
+        if not isinstance(threshold, list):
+            y_pred_los = reverse_los(preds[:,1], los_info)
+            y_true_los = reverse_los(labels[:,1], los_info)
+            return get_binary_metrics(preds[:,0], labels[:,0]) | get_regression_metrics(y_pred_los, y_true_los) | osmae_score(labels[:,0], y_true_los, preds[:,0], y_pred_los, large_los, threshold) | es_score(labels[:,0], y_true_los, preds[:,0], threshold)
+        else:
+            y_pred_los = reverse_los(preds[:,1], los_info)
+            y_true_los = reverse_los(labels[:,1], los_info)
+            osmae_list = []
+            es_list = []
+            for thr in threshold:
+                osmae_list.append(osmae_score(labels[:,0], y_true_los, preds[:,0], y_pred_los, large_los, thr)['osmae'])
+                es_list.append(es_score(labels[:,0], y_true_los, preds[:,0], thr)['es'])
+            return {"osmae_list": osmae_list, "es_list": es_list}
     else:
         raise ValueError("Task not supported")
