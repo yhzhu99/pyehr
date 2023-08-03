80d85718f5489088a25619ae44f92719a0091824
Yinghao Zhu <yhzhu99@gmail.com>
2023-08-03 19:38:32 +0800

    fix: auroc/auprc do not need to pass threshold

--- metrics/binary_classification_metrics.py
+++ metrics/binary_classification_metrics.py
@@ -6,8 +6,8 @@ threshold = 0.5
 def get_binary_metrics(preds, labels):
 
     accuracy = Accuracy(task="binary", threshold=threshold)
-    auroc = AUROC(task="binary", threshold=threshold)
-    auprc = AveragePrecision(task="binary", threshold=threshold)
+    auroc = AUROC(task="binary")
+    auprc = AveragePrecision(task="binary")
 
     # convert labels type to int
     labels = labels.type(torch.int)
